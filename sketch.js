b900bed9ff7c3e5b58ef5eef8bc96009d61e3ea8
Darshana <dp2556@nyu.edu>
2021-02-17 13:07:29 -0500

    first commit

--- sketch.js
+++ sketch.js
@@ -5,19 +5,8 @@ var tilesAcross = 10;
 var tileSize;
 var shapes = [];
 
-var slider1;
-var slider2;
-var button;
-var pause= true;
-
 function setup() {
 	createCanvas(windowWidth, windowHeight);
-
-	slider1 = createSlider(0,150, 0); //min, max, value it starts at, step aka increment
-	slider2 = createSlider(0.03,0.2,0.03, 0.01);
-
-	button = select('button');
-	button.mousePressed(pausePlay);
 
 	tileSize = width / tilesAcross;
 
@@ -33,9 +22,7 @@ function draw() {
 
 	for (var i = 0; i < shapes.length; i++) {
 		shapes[i].view();
-		if (pause) {
 		shapes[i].changeColor();
-		}
 	}
 }
 
@@ -54,29 +41,23 @@ class Bricks {
 		var brickColor = lerpColor(this.startColor, this.endColor, cos(this.colorValue))
 		var brickColor2 = lerpColor(this.endColor, this.startColor, cos(this.colorValue))
 
-		var newtileSize = tileSize + slider1.value();
-
 		//vertical
 		fill(brickColor);
-		rect(this.x, this.y, newtileSize / 2, newtileSize / 2);
-		rect(this.x + (newtileSize / 2), this.y, newtileSize / 2, newtileSize / 2);
+		rect(this.x, this.y, tileSize / 2, tileSize / 2);
+		rect(this.x + (tileSize / 2), this.y, tileSize / 2, tileSize / 2);
 		//horizontal		
 		fill(brickColor2);
-		rect(this.x, this.y + (newtileSize / 2), newtileSize, newtileSize / 2);
-		rect(this.x, this.y + (newtileSize * 0.75), newtileSize, newtileSize / 2);
+		rect(this.x, this.y + (tileSize / 2), tileSize, tileSize / 2);
+		rect(this.x, this.y + (tileSize * 0.75), tileSize, tileSize / 2);
 
 	}
 
 	changeColor() {
-		this.colorValue += slider2.value();
+		this.colorValue += .03;
 	}
 }
 
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
-}
-
-function pausePlay() {
-pause = !pause;
 }
 
